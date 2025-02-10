20fed07c48469e3dd54e213cfaba3861ce13317d
corinceSub <hanwang3@andrew.cmu.edu>
2025-02-10 11:20:07 -0500

    use cc_dat_utils to make pfgd_test.txt

--- part_1_read_test_dat.py
+++ part_1_read_test_dat.py
@@ -1,8 +1,17 @@
 import cc_dat_utils
 
-#Part 1
+# Part 1: Load the DAT file and print its string representation
 input_dat_file = "data/pfgd_test.dat"
 
+# Use cc_dat_utils.make_cc_level_pack_from_dat() to load the file specified by input_dat_file
+level_pack = cc_dat_utils.make_cc_level_pack_from_dat(input_dat_file)
 
-#Use cc_dat_utils.make_cc_level_pack_from_dat() to load the file specified by input_dat_file
-#print the resulting data
+# Print the string representation of the level_pack
+print(str(level_pack))
+
+# Save the output to a text file in the data folder
+with open("data/pfgd_test.txt", "w") as output_file:
+    output_file.write(str(level_pack))
+
+# You would need to manually load the pfgd_test.dat file in TileWorld and take a screenshot at this point.
+# After taking the screenshot, save it as pfgd_test_screenshot.png or .jpg in the data folder.
